7452306e5c4107c0b3f7b2f55536d7aca506cdcd
Rahul <rahulkrishna1595@gmail.com>
2025-12-15 00:11:45 +0530

    pendings done

--- controllers/user/checkoutUserController.js
+++ controllers/user/checkoutUserController.js
@@ -96,7 +96,7 @@ const loadCheckout = asyncHandler( async( req,res) => {
     const isRetry = req.query.retry ==='1'
     const lastFailedOrderId = req.session.lastFailedOrderId || null
 
-    res.render('user/checkout',{layout:'layouts/user_main',walletBalance,addresses,coupons,cart:{...cart,items:validItems},actualPrice:actualPrice.toFixed(2),offerDiscount:offerDiscount.toFixed(2),subtotal:subtotalAfterOffer.toFixed(2),couponDiscount:couponDiscount.toFixed(2),totalDiscount:totalDiscount.toFixed(2),tax:tax.toFixed(2),total:total.toFixed(2),appliedCoupon,cartLength:validItems.length || 0,selectedId:req.query.selected || null,error:req.query.error || null,retry:isRetry,retryOrderId:lastFailedOrderId})
+    res.render('user/checkout',{layout:'layouts/user_main',walletBalance,addresses,coupons,cart:{...cart,items:validItems},actualPrice:actualPrice.toFixed(2),offerDiscount:offerDiscount.toFixed(2),subtotal:subtotalAfterOffer.toFixed(2),couponDiscount:couponDiscount.toFixed(2),totalDiscount:totalDiscount.toFixed(2),tax:tax.toFixed(2),total:total.toFixed(2),appliedCoupon,cartLength:validItems.length || 0,selectedId:req.query.selected || null,error:req.query.error || null,success:req.query.success || null,retry:isRetry,retryOrderId:lastFailedOrderId})
 })
 
 const placeOrder = asyncHandler( async( req,res) => {
